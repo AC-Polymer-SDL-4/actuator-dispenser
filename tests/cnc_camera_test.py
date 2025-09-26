8e86f05b206b622a524670746d6083120296f4f3
Owen Alfred Melville <157226448+owen-melville@users.noreply.github.com>
2025-09-26 10:30:21 -0400

    testing reservoir and camera

---
+++ tests/cnc_camera_test.py
@@ -0,0 +1,46 @@
+from base_workflow import Liquid_Dispenser, start_workflow_logging
+import time
+import os
+import pandas as pd
+import datetime
+
+# Get workflow name (file name without extension)
+workflow_name = os.path.splitext(os.path.basename(__file__))[0]
+timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+output_dir = os.path.join("output", workflow_name, timestamp)
+
+# Initialize dispenser in virtual mode
+virtual = False
+dispenser = Liquid_Dispenser(cnc_comport="COM4", 
+                             actuator_comport="COM3", 
+                             camera_index=1, 
+                             virtual=virtual,
+                             output_dir=output_dir)
+dispenser.cnc_machine.Z_LOW_BOUND = -70 #Just in this case
+
+dispenser.cnc_machine.home()
+
+df = pd.DataFrame()
+
+for i in range(12):
+    try:
+        r, g, b = dispenser.get_image_rgb("well_plate_camera", i, f"_{i}")
+        # Add RGB values to the existing DataFrame
+        df.loc[i, 'Red'] = r
+        df.loc[i, 'Green'] = g
+        df.loc[i, 'Blue'] = b
+
+    except Exception as e:
+        if not virtual:
+            print(f"Cannot capture image for well {i}: {e}")
+        continue
+
+    dispenser.camera.cleanup()
+
+# Save DataFrame to CSV
+if not virtual:
+    print (df)
+    output_csv = "well_plate_data.csv"
+    df.to_csv(os.path.join(output_dir,output_csv), index=False) 
+    # Save DataFrame to CSV
+    print(f"Data saved to {output_csv}")
--- tests/cnc_test.py
+++ tests/cnc_test.py
@@ -1,15 +1,21 @@
 from base_workflow import CNC_Machine
 
-cnc = CNC_Machine(com = "COM4")
+cnc = CNC_Machine(com = "COM4:", z_low_bound=-70)
 
 #option 1: Hard controls
-cnc.move_to_point(x=5, y=10, z=-25)
-cnc.move_to_point(0,0,5)
-cnc.move_to_point(z=0) #Use for this
+# cnc.move_to_point(x=5, y=10, z=-25)
+# cnc.move_to_point(0,0,5)
+# cnc.move_to_point(z=0) #Use for this
 
-#Location-based controls... Does the calculation for you
-cnc.move_to_location('vial_rack', 0) #where? What position?
+# #Location-based controls... Does the calculation for you
+# cnc.move_to_location('vial_rack', 0) #where? What position?
 
-for i in range (0, 24):
-    cnc.move_to_location('well_plate', i, safe = False) #move to each well plate spot
+# for i in range (0, 24):
+#     cnc.move_to_location('well_plate', i, safe = False) #move to each well plate spot
 
+cnc.home()
+cnc.move_to_location('reservoir_12', 0, safe=True)
+# for i in range(0,12):
+#     cnc.move_to_location('reservoir_12', i, safe=True)
+#     input("Press enter to go to next slot:")
+cnc.close()
