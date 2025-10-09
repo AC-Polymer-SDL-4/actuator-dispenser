ec0beacd74181c5f360d784f415f42b52d38b0ff
owen-melville <owen.melville@utoronto.ca>
2025-10-09 17:34:31 -0400

    asd

--- tests/act_ext.py
+++ tests/act_ext.py
@@ -1,5 +1,8 @@
-from actuator_controller import ActuatorRemote
+from base_workflow import ActuatorRemote
+import time, logging
 
-act = ActuatorRemote(port='COM3')
+act = ActuatorRemote(port="COM3", virtual=False, log_level=logging.DEBUG, timeout=5.0)
 
-act.extend(1)
+print("EXT:", act.extend(0.8, speed=65000))
+print("RET:", act.retract(5, speed=65000))
+print("STOP:", act.stop())
