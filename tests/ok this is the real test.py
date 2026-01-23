693ce50196ad073bb73db2b14f9e6d3b77e89a46
mn0912 <monique.ngan@mail.utoronto.ca>
2026-01-23 15:00:31 -0500

    clean up test files

--- tests/ok this is the real test.py
+++
@@ -1,6 +0,0 @@
-from cnc_machine import CNC_Machine
-
-cnc = CNC_Machine(com = "COM4")
-
-for i in range (0, 24):
-    cnc.move_to_location('well_plate', i, safe = False) #move to each well plate spot
